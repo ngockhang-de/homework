cb9d846592b5a229dc5f5f215e40c5b43e94118a
Ngoc Khang <ngockhang.vu11@gmail.com>
2020-07-04 14:32:59 +0300

    homework10

---
+++ hw10.py
@@ -0,0 +1,27 @@
+from aiohttp import web
+import requests
+
+
+async def weather(request):
+    url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total"
+    country = 'Ukraine'
+
+    querystring = {"country": country}
+
+    headers = {
+        'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
+        'x-rapidapi-key': "b8af9d6883mshd7e92b07860b620p1cf3aejsnaac060abdb6b"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = response.json()
+    text = f'{country}\n\nrecovered-{data["data"]["recovered"]}\n\ndeaths-{data["data"]["deaths"]}\n\nconfirmed-{data["data"]["confirmed"]}'
+
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.add_routes([web.get('/weather', weather)])
+
+if __name__ == '__main__':
+    web.run_app(app)
